92b6e06eef492dea4d71bb0d5438cd7f3011772c
yuye <962701261@qq.com>
2022-06-01 23:37:47 +0800

    修改分类优化

--- renren-fast-vue-master/src/views/modules/product/category.vue
+++ renren-fast-vue-master/src/views/modules/product/category.vue
@@ -21,11 +21,7 @@
           >
             Append
           </el-button>
-          <el-button
-            type="text"
-            size="mini"
-            @click="update(data)"
-          >
+          <el-button type="text" size="mini" @click="update(data)">
             Update
           </el-button>
           <el-button
@@ -47,23 +43,25 @@
       :before-close="handleClose"
     >
       <el-form
-        :model="menuForm"
+        :model="category"
+        :rules="rules"
         status-icon
-        ref="menuForm"
+        ref="category"
         label-width="100px"
-        class="demo-menuForm"
+        class="demo-category"
       >
         <el-form-item label="分类名称" prop="name">
-          <el-input type="text" v-model="menuForm.name"></el-input>
+          <el-input type="text" v-model="category.name" auto-complete="off" ></el-input>
         </el-form-item>
         <el-form-item label="图标" prop="icon">
-          <el-input type="text" v-model="menuForm.icon"></el-input>
+          <el-input type="text" v-model="category.icon"></el-input>
         </el-form-item>
         <el-form-item label="计量单位" prop="unit">
-          <el-input type="text" v-model="menuForm.productUnit"></el-input>
+          <el-input type="text" v-model="category.productUnit"></el-input>
         </el-form-item>
-          <el-button type="primary" @click="submitCategory(menuForm)">提交</el-button>
-          <el-button @click="dialogVisible=false">取消</el-button>
+        <el-form-item align="right">
+          <el-button type="primary" @click="submitCategory(category)">提交</el-button >
+          <el-button @click="dialogClose()">取消</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -73,50 +71,52 @@
 <script>
 export default {
   data() {
+    var checkname = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('年龄不能为空'));
+        }else{
+          callback();
+        }
+    };
     return {
       menu: [],
       defaultProps: {
         children: "children",
         label: "name",
       },
-      menuForm: {
-        catId:"",
+      category: {
+        catId: "",
         name: "",
         parentCid: 0,
-        children:[],
+        children: [],
         catLevel: 1,
         sort: 0,
-        icon:"",
+        icon: "",
         showStatus: 1,
-        productUnit:"",
-        productCount:"",
+        productUnit: "",
+        productCount: "",
       },
-      // rules: {
-      //   name: [{ validator: nut, trigger: "blur" }],
-      //   parentCid: [{ validator: validatePass2, trigger: "blur" }],
-      //   age: [{ validator: checkAge, trigger: "blur" }],
-      //   catLevel: [{ validator: checkAge, trigger: "blur" }],
-      //   sort: [{ validator: checkAge, trigger: "blur" }],
-      //   icon: [{ validator: checkAge, trigger: "blur" }],
-      //   productUnit: [{ validator: checkAge, trigger: "blur" }],
-      //   productCount: [{ validator: checkAge, trigger: "blur" }],
-      // },
+      rules: {
+        name: [
+            { validator: checkname, trigger: 'blur' }
+          ],
+      },
       //分级菜单表单弹框开关
       dialogVisible: false,
       //默认展开节点
       expandedKey: [],
       //分级菜单表单弹框类型
-      dialogType:"",
-      dialogTitle:"",
+      dialogType: "",
+      dialogTitle: "",
     };
   },
 
   methods: {
     //判断节点是否可拖拽
     allowDrop(draggingNode, dropNode, type) {
-        console.log(draggingNode, dropNode, type)
-        return true;y
-      },
+      console.log(draggingNode, dropNode, type);
+      return true;
+    },
     handleClose(done) {
       this.$confirm("确认关闭？")
         .then((_) => {
@@ -125,57 +125,74 @@ export default {
         .catch((_) => {});
     },
     //提交新增分级菜单表单
-    submitCategory(menuForm) {
-      console.log(menuForm)
-      if(this.dialogType=="add"){
-        this.$http({
-        url: this.$http.adornUrl("/product/category/save"),
-        method: "post",
-        data: this.$http.adornData(menuForm, false),
-      }).then(({data}) => {
-        this.getMenus();
-        this.expandedKey = [menuForm.parentCid];
-        this.$message({
-            type: "success",
-            message: "添加成功!",
-          });
-      }).catch(() => {
-          this.$message({
-            type: "error",
-            message: "添加失败",
-          });
+    submitCategory(category) {
+      console.log(category);
+      this.$refs['category'].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
         });
-        this.dialogVisible=false;
-        this.getMenus();
-        this.expandedKey = [menuForm.parentCid];
-      }else{
-        let {catId,name,icon,productUnit} = this.menuForm;
-        this.$http({
-        url: this.$http.adornUrl("/product/category/update"),
-        method: "post",
-        data: this.$http.adornData({catId,name,icon,productUnit}, false),
-      }).then(({data}) => {
-        this.getMenus();
-        this.expandedKey = [catId];
-        this.$message({
-            type: "success",
-            message: "修改成功!",
-          });
-      }).catch(() => {
-          this.$message({
-            type: "error",
-            message: "修改失败",
-          });
-        });
-        this.dialogVisible=false;
-        this.getMenus();
-        this.expandedKey = [menuForm.catId];
-      }
+      // if (this.dialogType == "add") {
+      //   this.$http({
+      //     url: this.$http.adornUrl("/product/category/save"),
+      //     method: "post",
+      //     data: this.$http.adornData(category, false),
+      //   })
+      //     .then(({ data }) => {
+      //       this.getMenus();
+      //       this.expandedKey = [category.parentCid];
+      //       this.$message({
+      //         type: "success",
+      //         message: "添加成功!",
+      //       });
+      //     })
+      //     .catch(() => {
+      //       this.$message({
+      //         type: "error",
+      //         message: "添加失败",
+      //       });
+      //     });
+      //   this.dialogVisible = false;
+      //   this.getMenus();
+      //   this.expandedKey = [category.parentCid];
+      // } else {
+      //   let { catId, name, icon, productUnit } = this.category;
+      //   this.$http({
+      //     url: this.$http.adornUrl("/product/category/update"),
+      //     method: "post",
+      //     data: this.$http.adornData({ catId, name, icon, productUnit }, false),
+      //   })
+      //     .then(({ data }) => {
+      //       this.getMenus();
+      //       this.expandedKey = [catId];
+      //       this.$message({
+      //         type: "success",
+      //         message: "修改成功!",
+      //       });
+      //     })
+      //     .catch(() => {
+      //       this.$message({
+      //         type: "error",
+      //         message: "修改失败",
+      //       });
+      //     });
+      //   this.dialogVisible = false;
+      //   this.getMenus();
+      //   this.expandedKey = [category.catId];
+      // }
     },
     //重置分级菜单表单
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
+    dialogClose() {
+      this.dialogVisible = false;
+      this.resetForm(this.category);
     },
+    resetForm() {
+        console.log(this.$refs);
+        this.$refs['category'].resetFields();
+      },
     //删除确认提醒框
     open(node, data) {
       this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
@@ -199,34 +216,34 @@ export default {
     },
     //新增按钮
     append(data) {
-      this.dialogType="add";
-      this.dialogTitle="添加分类";
+      this.dialogType = "add";
+      this.dialogTitle = "添加分类";
       this.dialogVisible = true;
-      this.menuForm.parentCid=data.catId;
-      this.menuForm.catLevel=data.catLevel+1;
-      this.menuForm.name="";
-      this.menuForm.icon="";
-      this.menuForm.productUnit="";
-      this.expandedKey=[data.catId]; 
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel + 1;
+      this.category.name = "";
+      this.category.icon = "";
+      this.category.productUnit = "";
+      this.expandedKey = [data.catId];
       console.log("append", data);
     },
     update(data) {
-      this.dialogType="update";
-      this.dialogTitle="修改分类";
+      this.dialogType = "update";
+      this.dialogTitle = "修改分类";
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        this.category = data.category;
+      });
       this.dialogVisible = true;
-      this.menuForm.name=data.name;
-      this.menuForm.catId=data.catId;
-      this.menuForm.icon=data.icon;
-      this.menuForm.productUnit=data.productUnit;
-      this.expandedKey=[data.catId]; 
+      this.category.name = data.name;
+      this.category.catId = data.catId;
+      this.category.icon = data.icon;
+      this.category.productUnit = data.productUnit;
+      this.expandedKey = [data.catId];
       console.log("update", data);
     },
-    menuTitle(){
-        if(this.dialogtype=="add"){
-          
-        }
-    },
-
     remove(node, data) {
       console.log("remove", node, data);
       var ids = [data.catId];
